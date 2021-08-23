7216ea5b140e6ea2437abbaeccd1c291b710a433
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-08-23 14:40:00 -0500

    add in spell-check R script

---
+++ scripts/spell-check.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+#
+# Run spell check and save results
+# Adapted from: https://github.com/AlexsLemonade/refinebio-examples/blob/33cdeff66d57f9fe8ee4fcb5156aea4ac2dce07f/scripts/spell-check.R
+
+library(magrittr)
+
+# Find .git root directory
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+
+# Read in dictionary
+dictionary <- readLines(file.path(root_dir, 'components', 'dictionary.txt'))
+
+# Add emoji to dictionary
+dictionary <- c(dictionary, spelling::spell_check_text("⚠️")$word)
+
+# The only files we want to check are Markdown files
+files <- list.files(root_dir, pattern = '\\.md$', recursive = TRUE, full.names = TRUE)
+
+
+
+# Run spell check
+spelling_errors <- spelling::spell_check_files(files, ignore = dictionary) %>%
+  data.frame() %>%
+  tidyr::unnest(cols = found) %>%
+  tidyr::separate(found, into = c("file", "lines"), sep = ":")
+
+# Print out how many spell check errors
+write(nrow(spelling_errors), stdout())
+
+# Save spell errors to file temporarily
+readr::write_tsv(spelling_errors, 'spell_check_errors.tsv')
